6d56898e2a2b3a8a08652c2dce0072db1476a56f
Jens Rottiers <jens.rottiers123@gmail.com>
2026-04-16 20:20:19 +0200

    Indentation

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -4,12 +4,12 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { Home } from "../pages/home/Home";
 import { Projects } from "../pages/projects/Projects";
 
-const DemosIndex        = lazy(() => import("../pages/demos/DemosIndex"));
-const WordCloud         = lazy(() => import("../pages/demos/word-cloud/WordCloud"));
-const FlowingParagraph  = lazy(() => import("../pages/demos/flowing-paragraph/FlowingParagraph"));
-const TypewriterStream  = lazy(() => import("../pages/demos/typewriter-stream/TypewriterStream"));
+const DemosIndex = lazy(() => import("../pages/demos/DemosIndex"));
+const WordCloud = lazy(() => import("../pages/demos/word-cloud/WordCloud"));
+const FlowingParagraph = lazy(() => import("../pages/demos/flowing-paragraph/FlowingParagraph"));
+const TypewriterStream = lazy(() => import("../pages/demos/typewriter-stream/TypewriterStream"));
 const MultilangParticles = lazy(() => import("../pages/demos/multilang/MultilangParticles"));
-const BalancedLabels    = lazy(() => import("../pages/demos/balanced-labels/BalancedLabels"));
+const BalancedLabels = lazy(() => import("../pages/demos/balanced-labels/BalancedLabels"));
 
 const Routing: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ const Routing: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
-<Route exact path="/demos" component={DemosIndex} />
+        <Route exact path="/demos" component={DemosIndex} />
         <Route exact path="/demos/word-cloud" component={WordCloud} />
         <Route exact path="/demos/flowing-paragraph" component={FlowingParagraph} />
         <Route exact path="/demos/typewriter-stream" component={TypewriterStream} />
