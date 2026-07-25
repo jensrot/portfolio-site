9390d3fc0b6db7326e48e98298fc9bc38974ca06
jensrot <jens.rottiers123@gmail.com>
2026-07-26 00:37:02 +0200

    docs: removing redundant comments for personal prefered JSDoc comment style

--- src/utils/random-number-between-two-values.ts
+++ src/utils/random-number-between-two-values.ts
@@ -1,8 +1,10 @@
 /**
- * @description Returns a random number between 2 values.
- * @param {number} [startValue=0] - Where you want the number to start.
- * @param {number} endValue - Where you want the number to end.
- * @returns {number} - Gives back the random number between 2 values.
+ * Returns a random number between 2 values.
+ * 
+ * @param {number} [startValue=0] Where you want the number to start.
+ * @param {number} endValue Where you want the number to end.
+ * @returns {number} Gives back the random number between 2 values.
+ * 
  */
 export const getRandomNumberBetweenTwoValues = (startValue: number = 0, endValue: number): number => {
     return Math.random() * (endValue - startValue) + startValue;
