bc74032df6acfa320aa12c96e34e462bd1e2e91f
jensrot <jens.rottiers123@gmail.com>
2026-07-25 23:37:18 +0200

    chore: rename custom analytics to pi-analytics

--- src/components/route-tracker/RouteTracker.tsx
+++ src/components/route-tracker/RouteTracker.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 import { useLocation } from "react-router-dom";
 
-import { track } from "../../utils/analytics";
+import { track } from "../../utils/pi-analytics";
 
 /**
  * Fires one analytics beacon per client-side route change.
